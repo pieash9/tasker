d2a591c3e25c36591f934b698fce26260fe46d9a
Md. Pieash Ahmed <pieash9@gmail.com>
2025-05-14 15:59:31 +0600

    added add task fn

--- src/components/task/TaskBoard.jsx
+++ src/components/task/TaskBoard.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
+import AddTaskModal from "./AddTaskModal";
 
 const TaskBoard = () => {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React Native",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+    tags: ["react", "javascript"],
+    priority: "High",
+    isFavorite: true,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  const handleAddTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+    setShowAddModal(false);
+  };
+
   return (
     <section className="mb-20" id="tasks">
+      {showAddModal && <AddTaskModal onSave={handleAddTask} />}
       <div className="container">
         {/* Search Box */}
         <div className="p-2 flex justify-end">
@@ -12,8 +32,8 @@ const TaskBoard = () => {
         </div>
         {/* Search Box Ends */}
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions />
-          <TaskList />
+          <TaskActions onAddClick={() => setShowAddModal(true)} />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
