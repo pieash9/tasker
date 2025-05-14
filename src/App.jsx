244ae0373a82e3042bf293626a9e5d467f42a796
Md. Pieash Ahmed <pieash9@gmail.com>
2025-05-14 07:36:19 +0600

    created component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import Header from "components/Header";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import TaskBoard from "./components/task/TaskBoard";
 
 const App = () => {
   return (
     <>
       <Header />
+      <div className="flex flex-col items-center justify-center">
+        <HeroSection />
+        <TaskBoard />
+      </div>
+      <Footer />
     </>
   );
 };
